77950dd5ab9b9eb6f82aaa48881b3bf2165f8e7a
wizzard95 <damian110995a@gmail.com>
2026-05-02 14:04:37 -0400

    config|ui: agregar elementos al userdropdown y darle estilo

--- FRONT/src/components/Navbar/UserDropDown.jsx
+++ FRONT/src/components/Navbar/UserDropDown.jsx
@@ -1,5 +1,37 @@
 const UserDropDown = () => {
-    return <p>UserDropDown</p>
+    return (
+        <div className="dropdown dropdown-end">
+            <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+            >
+                <div className="w-10 rounded-full">
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                        alt="Avatar"
+                    />
+                </div>
+            </div>
+            <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow"
+            >
+                <li>
+                    <a className="justify-between">
+                        Perfil
+                        <span className="badge">Nuevo</span>
+                    </a>
+                </li>
+                <li>
+                    <a className="justify-between">Configuración</a>
+                </li>
+                <li>
+                    <a className="justify-between">Cerrar sesión</a>
+                </li>
+            </ul>
+        </div>
+    )
 }
 
 export default UserDropDown
