a65cedea416b487a9120af3a63aa08b108368077
wizzard95 <damian110995a@gmail.com>
2026-05-02 15:12:55 -0400

    config|ui: crear archivo ../register, importarlo dentro de app y referenciarlo como ruta

--- FRONT/src/App.jsx
+++ FRONT/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router'
 import Layout from './layout/Layout'
 import Home from './pages/Home'
+import Register from './pages/Register'
 
 function App() {
     return (
         <Routes>
             <Route element={<Layout />}>
                 <Route path="/" element={<Home />}></Route>
+                <Route path="/register" element={<Register />}></Route>
             </Route>
         </Routes>
     )
---
+++ FRONT/src/pages/Register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+    return (
+        <div className="mt-16">
+            <h1 className="text-3xl font-bold text-center">Registrarse</h1>
+        </div>
+    )
+}
+
+export default Register
