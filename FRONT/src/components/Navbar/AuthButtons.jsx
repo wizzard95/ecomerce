937a322152364195530791310f66ca932995f5db
wizzard95 <damian110995a@gmail.com>
2026-05-02 12:59:51 -0400

    config|ui: agregar botones de crear cuenta e inicio de sesion y darles estilo

--- FRONT/src/components/Navbar/AuthButtons.jsx
+++ FRONT/src/components/Navbar/AuthButtons.jsx
@@ -1,4 +1,16 @@
 const AuthButtons = () => {
-    return <div>AuthButtons</div>
+    return (
+        <div className="py-4 flex justify-center items-center gap-4 flex-wrap">
+            <button className="btn btn-neutral btn-outline">
+                Crear Cuenta
+            </button>
+
+            <div className="hidden lg:block">|</div>
+
+            <button className="btn btn-neutral btn-outline">
+                Iniciar sesión
+            </button>
+        </div>
+    )
 }
 export default AuthButtons
