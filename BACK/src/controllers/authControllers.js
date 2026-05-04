b71acc4ec4097586999245e636b33ce10f19aef2
wizzard95 <damian110995a@gmail.com>
2026-05-04 01:11:05 -0400

    config|back: Generar un token con JWT(json web token), Envciar el token como una cookie hacia el navegador para conectar con el frontend

--- BACK/src/controllers/authControllers.js
+++ BACK/src/controllers/authControllers.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 import UserModel from '../models/UserModel.js'
 import { registerSchema } from '../schemas/authSchema.js'
 
@@ -29,8 +30,24 @@ export const registerUser = async (req, res) => {
             password: hashedPassword,
             isAdmin: isFirstUser,
         })
-        console.log(newUser)
-        res.json({ newUser: newUser })
+
+        //* Generar un token con JWT(json web token)
+        const token = jwt.sign({ userId: newUser._id }, JWT_SECRET, {
+            expiresIn: '1h',
+        })
+        console.log('TOKEN:', token)
+        // * header.payload.signature
+
+        // * Envciar el token como una cookie hacia el navegador
+        //* para conectar con el frontend
+        res.cookie('accessToken', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', // true
+            sameSite: process.env.NODE_ENV == 'production' ? 'none' : 'lax',
+            maxAge: 60 * 60 * 1000,
+        })
+            .status(201)
+            .json({ message: 'Usuario registrado con exito' })
     } catch (error) {
         res.json(error)
     }
