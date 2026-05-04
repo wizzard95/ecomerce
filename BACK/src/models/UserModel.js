98c99d6e4ffba143342f29ae3976aaaa33f0b9ab
wizzard95 <damian110995a@gmail.com>
2026-05-03 20:03:04 -0400

    config|back|DB: crear el modelo de usuario con los datos requeridos para guardarse en la base de datos

---
+++ BACK/src/models/UserModel.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+// * esto es lo que se guardara en bd
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minLength: 6,
+        maxLength: 254,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 6,
+        maxLength: 254,
+    },
+    username: {
+        type: String,
+        default: '',
+        required: true,
+        trim: true,
+        minLength: 3,
+        maxLength: 20,
+    },
+    isAdmin: {
+        type: boolean,
+        default: false,
+        required: true,
+    },
+})
+export default mongoose.model('User', UserSchema)
