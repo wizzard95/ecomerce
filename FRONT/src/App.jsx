1e57952fd242b6df3d1f3f3064eefeaf7d8cc537
wizzard95 <damian110995a@gmail.com>
2026-05-05 02:45:13 -0400

    config|auth: crear el contexto para la sesion de cada usuario con manejo de errores y validaciones para importarlo desde app

--- FRONT/src/App.jsx
+++ FRONT/src/App.jsx
@@ -3,16 +3,19 @@ import Layout from './layout/Layout'
 import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import { UserContextProvider } from './context/UserContext'
 
 function App() {
     return (
-        <Routes>
-            <Route element={<Layout />}>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/register" element={<Register />}></Route>
-                <Route path="/login" element={<Login />}></Route>
-            </Route>
-        </Routes>
+        <UserContextProvider>
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/register" element={<Register />}></Route>
+                    <Route path="/login" element={<Login />}></Route>
+                </Route>
+            </Routes>
+        </UserContextProvider>
     )
 }
 
---
+++ FRONT/src/context/UserContext.jsx
@@ -0,0 +1,59 @@
+import {
+    useContext,
+    createContext,
+    useState,
+    useEffect,
+    useCallback,
+} from 'react'
+
+export const UserContext = createContext({})
+
+export const UserContextProvider = ({ children }) => {
+    const [userInfo, setUserInfo] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    // * funcion para verificar la sesion del usuario
+    const checkSession = useCallback(async () => {
+        try {
+            /* //? const userData = await getProfileService()
+             * //? setUserInfo(userData)
+             * a traves de esta funcion haremos una peticion a nuestro backend
+             * para checar la informacion del usuario en especifico*/
+        } catch (error) {
+            console.log('No hay sesion activa: ', error)
+            setUserInfo({})
+        } finally {
+            setLoading(false)
+        }
+    }, [])
+    // * funcion para obtener el id del usuario autenticado
+    const getUserId = () => {
+        return userInfo?.id || null
+    }
+
+    //* verificar si el usuario esta autenticado o no
+    const isAuthenticated = () => {
+        return !!userInfo?.id
+    }
+
+    useEffect(() => {
+        checkSession()
+    }, [checkSession])
+
+    return (
+        <UserContext.Provider
+            value={{
+                userInfo,
+                setUserInfo,
+                loading,
+                checkSession,
+                getUserId,
+                isAuthenticated,
+            }}
+        >
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export const useUser = () => useContext(UserContext)
