f7223df3ed1d66c9893fd766eac141004856784f
wizzard95 <damian110995a@gmail.com>
2026-05-07 15:34:05 -0400

    config|ui|auth: ocultar el userDropDown para cuando el usuario no esté logueado

--- FRONT/src/components/Navbar/Navbar.jsx
+++ FRONT/src/components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
                     )}
 
                     <Cart />
-                    <UserDropDown />
+                    {!loading && userInfo?.username && <UserDropDown />}
                 </div>
             </nav>
         </header>
