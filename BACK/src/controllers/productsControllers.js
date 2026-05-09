079629582b7c1ada5b48a536f2c6b36658ec3ad6
wizzard95 <damian110995a@gmail.com>
2026-05-08 20:20:08 -0400

    feat: terminar la funcion de crear producto, aplicar validaciones desde productSchema, mapear errores con zod y enviar datos al frontend

--- BACK/src/controllers/productsControllers.js
+++ BACK/src/controllers/productsControllers.js
@@ -1,11 +1,14 @@
 import { productSchema } from '../schemas/productSchema.js'
 import ProductModel from '../models/ProductModel.js'
+import { ZodError } from 'zod'
 
 export const createProduct = async (req, res) => {
     try {
+        // * traer los datos del producto
         const { name, description, price, stock, imageUrl } =
             productSchema.parse(req.body)
 
+        //* creamos el producto
         const product = await ProductModel.create({
             name,
             description,
@@ -14,16 +17,17 @@ export const createProduct = async (req, res) => {
             imageUrl,
         })
 
-        res.json({
-            data: {
-                name,
-                description,
-                price,
-                stock,
-                imageUrl,
-            },
-        })
+        //* devolvemos el producto al frontend
+        return res
+            .status(201)
+            .json({ message: 'Producto creado exitosamente', product })
     } catch (error) {
-        res.json({ message: 'Error al crear producto' })
+        if (error instanceof ZodError) {
+            return res
+                .status(400)
+                .json(error.issues.map((issue) => ({ message: issue.message })))
+        }
+
+        return res.status(500).json({ message: 'Error al crear el producto' })
     }
 }
