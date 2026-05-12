e42df67e30888b822735a17fc19551ec24b22f58
wizzard95 <damian110995a@gmail.com>
2026-05-12 15:30:25 -0400

    feat|ui: crear el contexto y la funcion para traer todos los productos al front

---
+++ FRONT/src/context/ProductContext.jsx
@@ -0,0 +1,49 @@
+import {
+    useState,
+    useEffect,
+    useCallback,
+    createContext,
+    useContext,
+} from 'react'
+import axios from 'axios'
+
+axios.defaults.withCredentials = true
+
+const API_URL = import.meta.env.VITE_BACKEND_URL + '/products'
+
+export const ProductContext = createContext({})
+
+export const ProductContextProvider = ({ children }) => {
+    const [products, setProducts] = useState([])
+    const [productsLoading, setproductsLoading] = useState([])
+    const [product, setProduct] = useState([])
+    const [productLoading, setProductLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    // * funcion para traer todos los productos al front
+    const getProducts = useCallback(async () => {
+        try {
+            const response = await axios.get(API_URL)
+            setProducts(response.data)
+        } catch (error) {
+            setError(error.message || 'Error al obtener los productos')
+        } finally {
+            setproductsLoading(false)
+        }
+    }, [])
+    useEffect(() => {
+        getProducts()
+    }, [getProducts])
+
+    const value = {
+        products,
+        product,
+        productsLoading,
+        productLoading,
+        error,
+        getProducts,
+    }
+    return <ProductContext.Provider value={value}></ProductContext.Provider>
+}
+//* hook personalizado para traer valores (values)
+export const useProduct = () => useContext(ProductContext)
