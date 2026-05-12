421c303dedf41d7a309a004d69f72703f7831e3f
wizzard95 <damian110995a@gmail.com>
2026-05-12 17:55:57 -0400

    config|ui: crear Link para ver el detalle del producto

--- FRONT/src/App.jsx
+++ FRONT/src/App.jsx
@@ -6,6 +6,7 @@ import Login from './pages/Login'
 import { UserContextProvider } from './context/UserContext'
 import { Toaster } from 'react-hot-toast'
 import { ProductContextProvider } from './context/ProductContext'
+import DetailProduct from './pages/DetailProduct'
 
 function App() {
     return (
@@ -16,6 +17,10 @@ function App() {
                         <Route path="/" element={<Home />}></Route>
                         <Route path="/register" element={<Register />}></Route>
                         <Route path="/login" element={<Login />}></Route>
+                        <Route
+                            path="/detailProduct/:id"
+                            element={<DetailProduct />}
+                        ></Route>
                     </Route>
                 </Routes>
             </ProductContextProvider>
---
+++ FRONT/src/pages/DetailProduct.jsx
@@ -0,0 +1,5 @@
+const DetailProduct = () => {
+    return <h1>DETAIL PRODUCT PAGE</h1>
+}
+
+export default DetailProduct
